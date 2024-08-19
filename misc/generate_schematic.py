08a60b11e26ab30af1017f7e7337307fc15fdb5b
arahangua <arahangua@gmail.com>
2024-08-19 18:10:53 +0200

    refactor: simplify schematic generation by updating AWS components and removing Graphviz check

--- misc/generate_schematic.py
+++ misc/generate_schematic.py
@@ -1,39 +1,32 @@
-import shutil
-import sys
 from diagrams import Diagram, Cluster, Edge
 from diagrams.custom import Custom
-from diagrams.generic.compute import Rack
-from diagrams.generic.database import SQL
-from diagrams.generic.ml import ML
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.ml import Sagemaker
 from diagrams.onprem.compute import Server
 from diagrams.onprem.client import Users
 from diagrams.onprem.network import Internet
 
-# Check if Graphviz is installed
-if not shutil.which("dot"):
-    print("Error: Graphviz is not installed or not found in PATH.", file=sys.stderr)
-    sys.exit(1)
-
 with Diagram("Semantic Graph Reinforcement Learning Architecture", show=False, direction="LR"):
-    users = Users("Users")
-    internet = Internet("Internet")
-
-    with Cluster("Graph Construction"):
-        graph_db = SQL("Knowledge Graph DB")
-        graph_construction = Server("Graph\nConstruction\nService")
-
-    with Cluster("Graph Embedding"):
-        graph_embedding = ML("Graph\nEmbedding\nModel")
-
-    with Cluster("Policy Learning"):
-        policy_learning = ML("Policy\nLearning\nModel")
-
-    with Cluster("Environment Interaction"):
-        env_interaction = Rack("Environment\nInteraction\nService")
-
-    with Cluster("Evaluation"):
-        evaluation = Server("Evaluation\nService")
-
-    users >> internet >> graph_construction >> graph_db
-    graph_db >> graph_embedding >> policy_learning >> env_interaction >> evaluation
-    evaluation >> users
+    users = Users("Users")                                                                                                                                                                      
+    internet = Internet("Internet")                                                                                                                                                             
+                                                                                                                                                                                                 
+    with Cluster("Graph Construction"):                                                                                                                                                         
+        graph_db = SQL("Knowledge Graph DB")                                                                                                                                                    
+        graph_construction = Server("Graph\nConstruction\nService")                                                                                                                             
+                                                                                                                                                                                                
+    with Cluster("Graph Embedding"):                                                                                                                                                            
+        graph_embedding = ML("Graph\nEmbedding\nModel")                                                                                                                                         
+                                                                                                                                                                                                
+    with Cluster("Policy Learning"):                                                                                                                                                            
+        policy_learning = ML("Policy\nLearning\nModel")                                                                                                                                         
+                                                                                                                                                                                                
+    with Cluster("Environment Interaction"):                                                                                                                                                    
+        env_interaction = Rack("Environment\nInteraction\nService")                                                                                                                             
+                                                                                                                                                                                                
+    with Cluster("Evaluation"):                                                                                                                                                                 
+        evaluation = Server("Evaluation\nService")                                                                                                                                              
+                                                                                                                                                                                                 
+    users >> internet >> graph_construction >> graph_db                                                                                                                                         
+    graph_db >> graph_embedding >> policy_learning >> env_interaction >> evaluation                                                                                                             
+    evaluation >> users 
